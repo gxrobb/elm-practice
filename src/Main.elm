32c0d2db3767c8cd3e65ff3ecbe1ce0f709d6af1
Robert Kregloh <gxrobb@gmail.com>
2021-08-04 11:15:22 -0400

    Minimum requirements quite literally met

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,7 @@ import Html.Attributes exposing (..)
 import Quote
 
 
+
 main =
   Browser.sandbox
     {
@@ -21,39 +22,50 @@ main =
 
 type Messages =
   Change
-    | ChangeTheName String
+    | SetNewName String
+    | SetDeleteName String
     | DeleteTheName
-    | GetQuote
+    | Add
+    | Delete
+    | SetUpdateText String
+    -- | GetQuote
 
 init =
   {
     name = "Robb"
     , newNameValue = ""
-    , quote = ""
+    , updateMe = "Change Me!"
+    , nameList = ["Robb", "James"]
+    , deleteNameValue = ""
   }
 
-view model =
-  div [] [
-    h1 [] [text "Hello Datica! My Name Is: ", text model.name]
-    , div [][text "Change Name?"]
-    , div [][
-        input [onInput ChangeTheName][]
-        , button [onClick Change][text "Change it!"]
-        , button [onClick DeleteTheName][text "Delete the name!"]
-    ]
-    ,
-    div [][
-      h2 [][text "Quote Request"]
-      , p [][text model.quote]
-      , button [onClick GetQuote][text "Requiest Quote"]
-    ]
-  ]
 
+view model =
+  div []
+    [ ul [] (List.map viewName model.nameList)
+    , input [onInput SetNewName] []
+    , button [onClick Add] [text "Add Name"]
+    , input [onInput SetDeleteName] []
+    , button [onClick Delete] [text "Delete Name"]
+    , div [][
+      text model.updateMe
+      , div [][
+        input [onInput SetUpdateText] []
+        ]
+      ]
+    ]
+
+viewName name =
+  li [] [ text name ]
 
 changeIt text =
   if text /= "" then text else "Please Enter A Name"
 
 
+-- toBeDeleted name = List.member name ["Robb", "James", "Matt"]
+-- List.filter (not << toBeDeleted) [ "Robb", "William", "James", "Bernard", "Dolores", "Teddy" ]
+--
+--
 
 
 
@@ -61,9 +73,15 @@ update msg model =
   case msg of
     Change ->
       { model | name = changeIt model.newNameValue }
-    ChangeTheName theText ->
+    SetNewName theText ->
       { model | newNameValue = theText}
+    SetDeleteName theText ->
+      { model | deleteNameValue = theText}
     DeleteTheName ->
       { model | name = "" }
-    GetQuote ->
-      { model | quote = "3" }
+    Add ->
+      { model | nameList = model.nameList ++ [ model.newNameValue ] }
+    Delete  ->
+      { model | nameList = List.filter (\name -> name /= model.deleteNameValue) model.nameList }
+    SetUpdateText theText ->
+      { model | updateMe = theText}
