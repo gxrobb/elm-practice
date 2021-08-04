335f379444f2b36fb09f8ebacc810c9783290933
Robert Kregloh <gxrobb@gmail.com>
2021-08-03 23:39:44 -0400

    temp stop point

---
+++ src/Main.elm
@@ -0,0 +1,70 @@
+module Main exposing(..)
+
+import Browser
+import String exposing (..)
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Quote
+
+main =
+  Browser.element
+    {
+      init = init,
+      view = view,
+      update = update,
+      subscriptions = Quote.subscriptions
+    }
+
+
+
+
+
+type Messages =
+  Change
+    | ChangeTheName String
+    | DeleteTheName
+    | GetQuote
+
+init =
+  {
+    name = "Robb"
+    , newNameValue = ""
+    , quote = Quote.init
+  }
+
+view model =
+  div [] [
+    h1 [] [text "Hello Datica! My Name Is: ", text model.name]
+    , div [][text "Change Name?"]
+    , div [][
+        input [onInput ChangeTheName][]
+        , button [onClick Change][text "Change it!"]
+        , button [onClick DeleteTheName][text "Delete the name!"]
+    ]
+    ,
+    div [][
+      h2 [][text "Quote Request"]
+      , p [][text model.quote]
+      , button [onClick GetQuote][text "Requiest Quote"]
+    ]
+  ]
+
+
+changeIt text =
+  if text /= "" then text else "Please Enter A Name"
+
+
+
+
+
+update msg model =
+  case msg of
+    Change ->
+      { model | name = changeIt model.newNameValue }
+    ChangeTheName theText ->
+      { model | newNameValue = theText}
+    DeleteTheName ->
+      { model | name = "" }
+    GetQuote ->
+      { model | quote = Quote.GotText }
